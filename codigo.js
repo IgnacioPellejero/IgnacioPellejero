02b26a9440d385eb9bb7e3f781a142867bda449a
Ignacio Pellejero <126637114+IgnacioPellejero@users.noreply.github.com>
2023-08-12 11:12:01 -0300

    tercera preentrega

--- codigo.js
+++ codigo.js
@@ -30,96 +30,157 @@
 // }
 //) primera preentrega
 
-const shopConten= document.getElementById("shopConten");
-const verCarrito= document.getElementById("verCarrito")
-const modalContainer = document.getElementById("modal-container")
+const shopConten = document.getElementById("shopConten");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 const productos = [
     {
         id: 1,
         nombre: "wiscky",
         precio: 900,
-        img: "assets/Johnie Walker Blue Label.jpeg"
+        img: "assets/Johnie Walker Blue Label.jpeg",
+        cantidad: 1,
     },
     {
         id: 2,
         nombre: "cerveza",
         precio: 100,
-        img: "assets/Lugares con cerveza artesanal en la CDMX.jpeg"
+        img: "assets/Lugares con cerveza artesanal en la CDMX.jpeg",
+        cantidad: 1,
     },
     {
         id: 3,
         nombre: "vino",
         precio: 600,
-        img: "assets/Luigi Bosca line_ New labels.jpeg"
+        img: "assets/Luigi Bosca line_ New labels.jpeg",
+        cantidad: 1,
     },
     {
         id: 4,
         nombre: "espumante",
         precio: 700,
-        img: "assets/Champagne label design_ Дизайн этикетки для шампанского.jpeg"
+        img: "assets/Champagne label design_ Дизайн этикетки для шампанского.jpeg",
+        cantidad: 1,
     },
 ];
 
-let carrito = [];
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
 
 productos.forEach((product) => {
-let content = document.createElement("div");
-content.innerHTML=`
+    let content = document.createElement("div");
+    content.innerHTML = `
 <img src="${product.img}">
 <h2>${product.nombre}</h2>
 <p>${product.precio} $</p> 
 `
-shopConten.append(content)
+    shopConten.append(content)
 
-let comprar = document.createElement("button");
-comprar.innerText="comprar";
-content.append(comprar);
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    content.append(comprar);
 
-comprar.addEventListener("click" , ()=>{
-    carrito.push({
-        id:product.id,
-        img:product.img,
-        nombre:product.nombre,
-        precio:product.precio,
+    comprar.addEventListener("click", () => {
+
+        const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id)
+
+        if (repeat) {
+            carrito.map((prod) => {
+                if (prod.id === product.id) {
+                    prod.cantidad++;
+                }
+            })
+        } else {
+            carrito.push({
+                id: product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            })
+            carritoCounter();
+            saveLocal();
+        }
     })
-    
 })
-})
-verCarrito.addEventListener("click",()=>{
-    modalContainer.innerHTML="";
-    modalContainer.style.display="block"
+
+
+
+const pintarCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "block"
     const modalHeader = document.createElement("div")
     modalHeader.className = "modal-header"
-    modalHeader.innerHTML=`
+    modalHeader.innerHTML = `
     <h1 class="modal-header">carrito.</h1>
     `;
-modalContainer.append(modalHeader);
-const modalbutton=document.createElement("h1")
+    modalContainer.append(modalHeader);
+    const modalbutton = document.createElement("h1")
 
-modalbutton.innerText="x";
-modalbutton.className="modal-header-button"
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button"
 
-modalbutton.addEventListener("click",()=>{modalContainer.style.display="none"})
+    modalbutton.addEventListener("click", () => { modalContainer.style.display = "none" })
 
-modalHeader.append(modalbutton)
+    modalHeader.append(modalbutton)
 
-carrito.forEach((product)=>{
-    let carritoContent = document.createElement("div")
-    carritoContent.className= "modal-content"
-    carritoContent.innerHTML =`
+    carrito.forEach((product) => {
+        let carritoContent = document.createElement("div")
+        carritoContent.className = "modal-content"
+        carritoContent.innerHTML = `
     <img src="${product.img}">
     <h2>${product.nombre}</h2>
-    <p>${product.precio} $</p> 
+    <p>${product.precio} $</p>
+    
+    <p>Cantidad: ${product.cantidad}</p>
+    <p>Total:${product.cantidad * product.precio}</p>
     `;
-    modalContainer.append(carritoContent)
+        modalContainer.append(carritoContent)
+
+        let eliminar = document.createElement("span")
+        eliminar.innerHTML = "❌";
+        eliminar.className = "delete-product";
+        carritoContent.append(eliminar)
+
+        eliminar.addEventListener("click", eliminarProducto)
     })
 
-    const total = carrito.reduce((acc,el)=> acc + el.precio,0)
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cantidad, 0)
 
     const totalBuying = document.createElement("div")
     totalBuying.className = "total-comtent"
     totalBuying.innerHTML = `total a pagar: ${total}$`;
     modalContainer.append(totalBuying)
-});
+
+};
+
+verCarrito.addEventListener("click", pintarCarrito)
+
+const eliminarProducto = () => {
+    const fundId = carrito.find((element) => element.id);
+
+    carrito = carrito.filter((carritoId) => {
+        return carritoId !== fundId;
+
+    })
+    carritoCounter();
+    saveLocal();
+    pintarCarrito();
+}
+
+const carritoCounter = () => {
+    cantidadCarrito.innerText = carrito.length
+
+    const carritoLengh = carrito.length
+
+    localStorage.setItem("carritoLength", JSON.stringify(carritoLengh))
+
+    cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLengh"))
+};
+carritoCounter()
+
+const saveLocal = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito))
+}
 
